15d5d5b15e738bc3e8eeabbe8a80447f8baf1b6c
Jdavidrb <u.jdavidrb@gmail.com>
2024-01-23 12:01:30 -0500

    New semester and add logic caprpet

---
+++ 3/logic-digital/simple-algoritm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Función para sumar números pares en una lista
+int sumarPares(vector<int> lista) {
+    int suma = 0;
+    for(int i = 0; i < lista.size(); i++) {
+        if(lista[i] % 2 == 0) {
+            suma += lista[i];
+        }
+    }
+    return suma;
+}
+
+int main() {
+    
+    vector<int> numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    
+    int sumaDePares = sumarPares(numeros);
+
+    
+    cout << "La suma de los números pares es: " << sumaDePares << endl;
+
+    return 0;
+}
